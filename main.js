aa2da01053fbb417f349318fe55e2c836b7bcbee
nahuel-fiocco <nahuelfiocco@gmail.com>
2023-03-27 21:24:28 -0700

    3ra pre entrega: JSON + Local Storage + DOM + Eventos

--- main.js
+++ main.js
@@ -1,12 +1,9 @@
 document.addEventListener("DOMContentLoaded", function () {
   document.getElementById("textLocation").focus();
-  const location = localStorage.getItem("location");
-  const temperature = localStorage.getItem("temperature");
-  if (location && temperature) {
-    document.getElementById(
-      "temperature"
-    ).textContent = `${location}: ${temperature} °C`;
-    document.getElementById("textLocation").value = location;
+  const weatherData = JSON.parse(localStorage.getItem("weatherData"));
+  if (weatherData) {
+    document.getElementById("temperature").textContent = `${weatherData.location}: ${weatherData.temperature} °C`;
+    document.getElementById("textLocation").value = `${weatherData.location}`;
   }
 });
 
@@ -20,13 +17,15 @@ enter.addEventListener("keypress", function (event) {
   }
 });
 
+const limpiarButton = document.getElementById("limpiar");
+limpiarButton.addEventListener("click", limpiar);
+
 function getTemperature() {
   const location = document.getElementById("textLocation").value;
-  if (!location) {
-    document.getElementById(
-      "temperature"
-    ).textContent = `Debes ingresar una ubicacion`;
-  } else {
+  if(!location){
+    document.getElementById("temperature").textContent = `Debes ingresar una ubicacion`
+  }
+  else{
     const apiKey = "19b74674a6c35f1883a818fb8e078fb0";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=metric`;
     fetch(url)
@@ -35,15 +34,30 @@ function getTemperature() {
         const temperature = data.main.temp;
         const temperatureElement = document.getElementById("temperature");
         temperatureElement.textContent = `${location}: ${temperature} °C`;
-        localStorage.setItem("location", location);
-        localStorage.setItem("temperature", temperature);
+
+        // Guardar los datos en el localStorage
+        const dataObj = { location, temperature };
+        localStorage.setItem('weatherData', JSON.stringify(dataObj));
       })
-      .catch(
-        () =>
-          (document.getElementById("temperature").textContent =
-            "Ubicaion invalida")
-      );
-    localStorage.removeItem("location");
-    localStorage.removeItem("temperature");
-  }
+      .catch(() => {
+        document.getElementById('temperature').textContent = "Ubicaion invalida";
+        localStorage.removeItem('weatherData');
+      });
+    }
+}
+
+function limpiar() {
+  // Borrar el valor almacenado en Local Storage
+  localStorage.removeItem("weatherData");
+  // Borrar el contenido de la etiqueta donde se muestran los resultados
+  document.getElementById("temperature").textContent = "";
+  document.getElementById("textLocation").value = "";
+}
+
+function saveToLocalStorage(location, temperature) {
+  const data = {
+    location: location,
+    temperature: temperature,
+  };
+  localStorage.setItem("weatherData", JSON.stringify(data));
 }
